108fd5f6743e1d4dfb5429231642c3af39c1d7b4
haysaa <148773118+haysaa@users.noreply.github.com>
2024-08-26 19:36:16 +0300

    Update SafeHouse.java

--- SafeHouse.java
+++ SafeHouse.java
@@ -1,18 +1,20 @@
 public class SafeHouse extends NormalLoc{
    private Player player;
-
+ public SafeHouse(Player player) {
+        super(player,"SafeHouse");
+    this.player=player;
+    }
     @Override
     boolean onLocation() {
         System.out.println("You are in the safehouse");
         regenerateHp();
         return true;
     }
-    public SafeHouse(Player player) {
-        super(player,"SafeHouse");
-    }
+   
     public void regenerateHp(){
         if(player.getHp()< player.getMaxhp()){
             player.setHp(player.getMaxhp());
+            System.out.println("HP regenerated");
         }
         else{
             System.out.println("You already have a hp of "+player.getMaxhp());
