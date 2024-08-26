dda39b26a9a0960c0c83d4acdff24f6288d569e5
haysaa <148773118+haysaa@users.noreply.github.com>
2024-08-26 20:10:24 +0300

    Update Cave.java

--- Cave.java
+++ Cave.java
@@ -2,6 +2,6 @@ public class Cave extends BattleLoc{
 
    private Player player;
    public Cave(Player player) {
-       super(player,"cave",new Zombie(),"food");
+       super(player,"cave",new Zombie(),"Food");
    }
 }
